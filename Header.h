18047ce775aceae84b8dc9917aaffad12fcbbed8
Pavel-Kirilenko <pavelkirilenko@mail.ru>
2024-11-14 23:34:39 +0400

    Добавить файл

---
+++ Header.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+#include <vector>
+void  totalSumCounter(int arrayLength, short partsNumber, std::vector <int> rndArray);  // Функция подсчета суммы элементов массива с использованием метода многопоточности
